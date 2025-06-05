bfd44254cebff2064f4ddb2dd8e7d2c9a271a906
caeklol <me@caek.dev>
2025-06-06 01:45:21 +0800

    impl [1]

--- src/tracer.rs
+++ src/tracer.rs
@@ -1,24 +1,32 @@
 use std::{net::IpAddr, sync::{mpsc::{self, Sender, TryRecvError}, Arc, RwLock}, thread::{self}, time::Duration};
 
-#[derive(Clone, Copy)]
+// core of tracert. i could have probably used an external lib yes but this is so much cooler and
+// so much more aura
+
+fn do_probe(ip: IpAddr, ttl: usize, timeout: Duration) -> Option<(IpAddr, Duration)> {
+    None
+}
+
+#[derive(Clone)]
 pub struct Node {
     ip: IpAddr,
-    latency: Duration,
-    n: usize
+    latency: Duration
 }
 
 #[derive(Clone)]
 pub struct TraceState {
     nodes: Vec<Node>,
-    ttl: u32,
+    ttl: usize,
 }
 
 pub struct TraceHandler {
+    callback: Arc<dyn Fn() + Send + Sync + 'static>,
+    state: Arc<RwLock<Option<TraceState>>>,
+    cancel: Option<Sender<()>>,
     tracing: bool,
     ip: Option<IpAddr>,
-    cancel: Option<Sender<()>>,
-    callback: Arc<dyn Fn() + Send + Sync + 'static>,
-    state: Arc<RwLock<Option<TraceState>>>
+    max_ttl: usize,
+    timeout: Duration
 }
 
 impl TraceHandler {
@@ -31,7 +39,9 @@ impl TraceHandler {
             ip: None,
             cancel: None,
             callback: Arc::new(callback),
-            state
+            state,
+            max_ttl: 0,
+            timeout: Duration::new(0, 0)
         }
     }
 
@@ -43,9 +53,16 @@ impl TraceHandler {
         self.ip = Some(ip);
     }
 
+    pub fn set_max_ttl(&mut self, max_ttl: usize) {
+        self.max_ttl = max_ttl;
+    }
+
+    pub fn set_timeout(&mut self, timeout: Duration) {
+        self.timeout = timeout;
+    }
 
     pub fn begin_trace(&mut self) {
-        if let Some(ip) = self.ip {
+        if let Some(ip) = self.ip.clone() {
             self.tracing = true;
 
             let (cancel_tx, cancel_rx) = mpsc::channel();
@@ -53,7 +70,11 @@ impl TraceHandler {
 
             let callback = Arc::clone(&self.callback);
             let state = Arc::clone(&self.state);
+            let max_ttl = self.max_ttl.clone();
+            let timeout = self.timeout.clone();
             thread::spawn(move || {
+                let mut ttl = 1;
+                let mut nodes = Vec::with_capacity(max_ttl);
                 loop {
                     match cancel_rx.try_recv() {
                         Ok(_) | Err(TryRecvError::Disconnected) => {
@@ -63,7 +84,33 @@ impl TraceHandler {
                         Err(TryRecvError::Empty) => {}
                     }
 
+                    if let Some(res) = do_probe(ip, ttl, timeout) {
+                        nodes.insert(ttl-1, Node {
+                            ip: res.0,
+                            latency: res.1
+                        });
+                    }
+
+                    match cancel_rx.try_recv() {
+                        Ok(_) | Err(TryRecvError::Disconnected) => {
+                            println!("recv cancel");
+                            break;
+                        }
+                        Err(TryRecvError::Empty) => {}
+                    }
+                    let mut w = state.write().unwrap();
+                    *w = Some(TraceState {
+                        nodes: nodes.to_owned(),
+                        ttl, 
+                    });
+
                     (callback)();
+
+                    if ttl < max_ttl {
+                        ttl += 1; 
+                    } else {
+                        break;
+                    }
                 }
             });
         } else {
